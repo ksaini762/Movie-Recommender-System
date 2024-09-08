f42197ae44645aaa329e9822acb2cb642e200383
Kanchan Saini <sainikanchan374@gmail.com>
2024-09-08 11:34:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,74 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+def fetch_poster(movie_id):
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=231eefe3ecdde5e29ee23832140df640'.format(movie_id))
+    data = response.json()
+    st.text(data)
+    st.text('https://api.themoviedb.org/3/movie/{}?api_key=231eefe3ecdde5e29ee23832140df640'.format(movie_id))
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+                 
+                  
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]   
+    distances = similarity[movie_index]  
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]  
+    recommended_movies = []
+    recommended_movies_posters = []
+    
+ 
+    for i in movie_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_movies_posters.append(fetch_poster(movie_id))
+    return recommended_movies,recommended_movies_posters
+
+
+
+        
+ 
+
+movies_dict = pickle.load(open('movie_dict.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
+ 
+
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+st.title('Movie recommender system')
+
+selected_movie_name = st.selectbox(
+    'hello, select a movie', 
+    movies['title'].values)
+
+if st.button('Recommend'):
+    names, posters = recommend(selected_movie_name)
+
+    col1, col2, col3, col4, col5 = st.columns(5) 
+    with col1:
+      st.header(names[0])
+      st.image(posters[0])
+
+    with col2:
+      st.header(names[1])
+      st.image(posters[1])
+
+    with col3:
+      st.header(names[2])
+      st.image(posters[2])
+
+    with col4:
+      st.header(names[3])
+      st.image(posters[3])
+
+    with col5:
+      st.header(names[4])
+      st.image(posters[4])
+
+     
+     
+    
+
+ 
